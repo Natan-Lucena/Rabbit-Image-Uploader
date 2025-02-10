a6fac3d4d17af3b1e040885ee33c02ea85bfcd19
Natan Lucena <zerogamerptbr@gmail.com>
2025-02-10 16:08:47 -0300

    :sparkles: feat: integrate PubSub service into SaveImageUseCase for image publishing

--- src/api/application/use-cases/save-image.ts
+++ src/api/application/use-cases/save-image.ts
@@ -2,6 +2,7 @@ import { Image } from "../../domain/entities/image";
 import { failure, Result, success } from "../../shared/core/result";
 import { ImageDto } from "../dtos/image-dto";
 import { FileStorage } from "../services/file-storage";
+import { PubSub } from "../services/pub-sub";
 
 interface Request {
   externalId: string;
@@ -9,7 +10,10 @@ interface Request {
 }
 
 export class SaveImageUseCase {
-  constructor(private fileStorage: FileStorage) {}
+  constructor(
+    private fileStorage: FileStorage,
+    private pubSubProvider: PubSub
+  ) {}
 
   async execute(
     data: Request
@@ -18,6 +22,10 @@ export class SaveImageUseCase {
     try {
       const { url } = await this.fileStorage.saveFile({ externalId, file });
       const image = Image.create(externalId, url);
+      await this.pubSubProvider.publish<ImageDto>("image", {
+        externalId: image.externalId,
+        url: image.url,
+      });
       return success({ externalId: image.externalId, url: image.url });
     } catch (error) {
       return failure("ERROR_SAVING IMAGE");
--- src/api/container/factory/use-case-factory.ts
+++ src/api/container/factory/use-case-factory.ts
@@ -1,9 +1,14 @@
 import { SaveImageUseCase } from "../../application/use-cases/save-image";
 import { AwsS3FileStorage } from "../../infrastructure/services/file-storage-impl";
+import { RabbitMQPublisher } from "../../infrastructure/services/pub-sub-impl";
 
 export class UseCaseFactory {
   static saveImageUseCase(): SaveImageUseCase {
     const fileStorage = new AwsS3FileStorage();
-    return new SaveImageUseCase(fileStorage);
+    if (!process.env.RABBITMQ_URL) {
+      throw new Error("RabbitMQ URL não definido");
+    }
+    const pubSub = new RabbitMQPublisher(process.env.RABBITMQ_URL);
+    return new SaveImageUseCase(fileStorage, pubSub);
   }
 }
